8fcc7391bb14f9b12986290f3dee0a676b6657ad
Connor Rose <connorrose.git@gmail.com>
2020-10-06 17:34:58 -0700

    add Sass styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
 
-const App = () => <h1>EXAMPLE</h1>;
+const App = () => {
+  const throwError = () => {
+    throw new Error();
+  };
+  return (
+    <>
+      <h1>EXAMPLE</h1>
+      <button type="button" onClick={throwError}>
+        do not press
+      </button>
+    </>
+  );
+};
 
 export default hot(App);
